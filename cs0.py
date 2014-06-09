238041d2c02730b2ecf14cc28ce25973e94df110
Graham <graham.montgomery@western.edu>
2014-06-09 13:16:29 -0600

    added window.capability and add(obj, xPos, yPos)

--- cs0.py
+++ cs0.py
@@ -13,19 +13,27 @@ import Engine
 from Colors import *
 from GObj import *
 
-g.WindowWidth = 500
-g.WindowHeight = 500
-g.WindowCaption = "Game Window"
-g.WindowColor = WHITE
+class _data:
+    def __init__(self):
+        pass
 
-def add(obj):
+window = _data()
+
+window.width = 500
+window.height = 500
+window.caption = "Game Window"
+window.color = WHITE
+
+def add(obj, xPos, yPos):
+    obj.x = xPos
+    obj.y = yPos
     g.world.add(obj)
 
 def remove(obj):
     g.world.remove(obj)
 
 def start():
-    Engine.start(g.WindowWidth, g.WindowHeight, g.WindowCaption, g.WindowColor)
+    Engine.start(window.idth, window.height, window.caption, window.color)
 
 def stop():
     Engine.stop()
